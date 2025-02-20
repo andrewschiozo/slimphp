df4ae027b17fc3c7c6019f7a36f433e168ccdbf2
Andrews Chiozo <andrewschiozo@gmail.com>
2025-02-20 16:20:07 -0300

    feat: implementação da entidade Sala

--- src/routes.php
+++ src/routes.php
@@ -11,3 +11,7 @@ $app->get('/agendamentos', [\src\Agendamento\Controllers\AgendamentoController::
 $app->get('/agendamentos/proximos', [\src\Agendamento\Controllers\AgendamentoController::class, 'listarProximos']);
 $app->get('/agendamento/{id}', [\src\Agendamento\Controllers\AgendamentoController::class, 'ver']);
 $app->post('/agendamento', [\src\Agendamento\Controllers\AgendamentoController::class, 'salvar'])->add(new JsonBodyParserMiddleware());
+
+$app->get('/salas', [\src\Sala\Controllers\SalaController::class, 'listar']);
+$app->get('/sala/{id}', [\src\Sala\Controllers\SalaController::class, 'ver']);
+$app->post('/sala', [\src\Sala\Controllers\SalaController::class, 'salvar'])->add(new JsonBodyParserMiddleware());
